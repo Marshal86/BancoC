f8b1bbe223ec1597bd9d462afa805d4550fa260a
Eduardo Marçal Clemente <80399639+Marshal86@users.noreply.github.com>
2022-06-06 21:11:14 -0300

    Add files via upload
    O Programa acima utiliza o console do Visual Studio e trata de um primeiro exercicio baseado em orientação a objeto.
    
    Descrição da atividade:
    Em um banco, para se cadastrar uma conta bancária, é necessário informar o número da conta, o nome do
    titular da conta, e o valor de depósito inicial que o titular depositou ao abrir a conta. Este valor de depósito
    inicial, entretanto, é opcional, ou seja: se o titular não tiver dinheiro a depositar no momento de abrir sua
    conta, o depósito inicial não será feito e o saldo inicial da conta será, naturalmente, zero.
    Importante: uma vez que uma conta bancária foi aberta, o número da conta nunca poderá ser alterado. Já
    o nome do titular pode ser alterado (pois uma pessoa pode mudar de nome por ocasião de casamento, por
    exemplo).
    Por fim, o saldo da conta não pode ser alterado livremente. É preciso haver um mecanismo para proteger
    isso. O saldo só aumenta por meio de depósitos, e só diminui por meio de saques. Para cada saque
    realizado, o banco cobra uma taxa de $ 5.00. Nota: a conta pode ficar com saldo negativo se o saldo não for
    suficiente para realizar o saque e/ou pagar a taxa.
    Você deve fazer um programa que realize o cadastro de uma conta, dando opção para que seja ou não
    informado o valor de depósito inicial. Em seguida, realizar um depósito e depois um saque, sempre
    mostrando os dados da conta após cada operação.

---
+++ Conta.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioBanco
+{
+    class Conta
+    {
+        
+        public int _numero_Conta;
+        public string Nome_Titular { get; set; }
+        private double _valor_Inicial;
+
+        public Conta(int numero_Conta, string nome_Titular)
+        {
+            _numero_Conta = numero_Conta;
+            Nome_Titular = nome_Titular;
+        }
+
+        public int Numero_Conta
+        {
+            get { return _numero_Conta; }
+            set
+            {
+                
+                do
+                {
+                    if (_numero_Conta == 0000)
+                    {
+                        _numero_Conta = value;
+
+                    }
+                    else { 
+                        Console.WriteLine("Não podemos alterar o valor da conta");
+                          };
+                   } while (!true);         
+            }
+        }
+        public double Valor_inicial
+        {
+            get {return _valor_Inicial; }
+
+            set
+            {
+                if (value >= 0.0)
+                {
+                    _valor_Inicial = value;
+                } else
+                {
+                   
+                       
+                  Console.WriteLine("Valor Informado não é maior que zero, favor informar novo valor");
+                    
+                    
+                }
+            }
+        }
+
+        public double Deposito (double deposito)
+        {
+            return Valor_inicial += deposito;
+        }
+
+
+        public double Saque (double saque)
+        {
+             Valor_inicial -= saque;
+            return Valor_inicial=Valor_inicial - 5;
+            
+        }
+
+        public override string ToString()
+        {
+            return "O Numero da conta é " + Numero_Conta + ", O Nome do Titular é: " + Nome_Titular + " e o valor inicial é: "+ Valor_inicial;
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ExercicioBanco
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+
+        {
+
+            Conta conta = new Conta(0000, "Cliente");
+            Console.WriteLine("informe o numero da conta");
+            conta.Numero_Conta = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("informe o nome do titular da conta");
+            conta.Nome_Titular = Console.ReadLine();
+
+            Console.WriteLine("informe o valor da conta");
+            conta.Valor_inicial = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+          
+
+            Console.WriteLine(conta);
+
+
+            Console.WriteLine("Entre um valor para depósito: ");
+            conta.Deposito(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Dados da conta atualizada: ");
+            Console.WriteLine(conta);
+
+
+            Console.WriteLine("Entre um valor para Saque ");
+            conta.Saque(double.Parse(Console.ReadLine()));
+
+            Console.WriteLine("Dados da conta atualizada: ");
+            Console.WriteLine(conta);
+
+
+        }
+    }
+}
